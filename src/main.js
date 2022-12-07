6be32910b1f9905e110d29f1ee27749c8b3fb4e1
Eduardo <eduardosgn19@gmail.com>
2022-12-07 18:56:53 -0300

    tailwindcss on dependencies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import './style.css';
 
 const form = document.querySelector('form');
+const textarea = document.querySelector('textarea');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
+
+    showSpinner();
 
     const data = new FormData(form);
 
@@ -17,8 +20,30 @@ form.addEventListener('submit', async (e) => {
         }),
     });
 
-    const { image } = await response.json();
+    if (response.ok) {
+        const { image } = await response.json();
+    
+        const result = document.querySelector('#result');
+        result.innerHTML = `<img src="${image}" class="border-solid border-2 border-stone-100" width="512" />`;
+    
+        textarea.value = '';
+    } else {
+        const error = await response.text();
+        alert(error);
+        console.log(error);
+    }
 
-    const result = document.querySelector('#result');
-    result.innerHTML = `<img src="${image}" class="border-solid border-2 border-stone-100" width="512" />`;
-})
+    hideSpinner();
+});
+
+function showSpinner() {
+    const button = document.querySelector('button');
+    button.disabled = true;
+    button.innerHTML = "He's thinking... <span class='spinner'>👨‍💻</span>"
+};
+
+function hideSpinner() {
+    const button = document.querySelector('button');
+    button.disabled = false;
+    button.innerHTML = "Send to the robot!"
+};
--- src/server.js
+++ src/server.js
@@ -17,16 +17,21 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
-    const prompt = req.body.prompt;
+    try {
+        const prompt = req.body.prompt;
 
-    const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: '512x512',
-    });
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '512x512',
+        });
 
-    const image = aiResponse.data.data[0].url;
-    res.send({ image });
+        const image = aiResponse.data.data[0].url;
+        res.send({ image });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.res.data.error.message || 'Something went wrong...')
+    }
 });
 
 app.listen(8080, () => console.log('Image made with OpenAI API on http://localhost:8080/dream'));
